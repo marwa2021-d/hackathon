4936090cec6366ceda8b7b626ad178e008a47350
AdiBarHalevi <adibar141@gmail.com>
2021-04-27 23:37:12 +0300

    commit

--- server/rourers/index.router.js
+++ server/rourers/index.router.js
@@ -2,10 +2,33 @@ const express = require("express")
 const router = express.Router()
 const dataObj = require('../models/dataObj.model')
 
+const data = {
+    originalData: {
+        link: "https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector",
+        lang: "english",
+        rawData: {
+            title: "Document.querySelector()",
+            content: "The Document method querySelector() returns the first Element within the document that matches the specified selector, or group of selectors. If no matches are found, null is returned."
+        },
+    },
+    translatedData: {
+        isTranslated: true,
+        lang: "hebrew",
+        rawData: {
+            title: "יקשדכחשגד",
+            content: `חנדגשלכחדנגשכלחגנשדלהחמדשלה 
+            שדלחהמשדהל חמש
+            ש,דחמדלףחהמ`
+        },
+    }
+}
+
+
 router
     .get("/", async (req, res) => {
         try {
-            res.send('fetch is work')
+            const ans = JSON.stringify(data)
+            res.send(ans)
         } catch (e) {
             res.status(400).send(e.message)
         }
