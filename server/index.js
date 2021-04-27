795ff1682007cf2b9d822a099a2cc671e150ee46
mor <mor6686@gmail.com>
2021-04-27 16:58:22 +0300

    hackaton_0.5

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const path = require('path');
 require('./db/mongoose');
 const route = require('./routers/index.router');
 
@@ -8,6 +9,11 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json())
 app.use(cors())
 app.use('/api/', route);
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log('listening to port ' + PORT)
